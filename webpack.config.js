8da4516f07ed7bbe3bf69484b24becb85c6625ca
corvusnigra <itemruby@gmail.com>
2021-05-12 00:07:25 +0300

    init

---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+const path = require('path');
+const NODE_ENV = process.env.NODE_ENV;
+const HTMLWebpackPlugins= require('html-webpack-plugin')
+
+module.exports = {
+    resolve: {
+        extensions: ['.js','jsx','ts', 'tsx', 'json']
+    },
+    mode: NODE_ENV ? NODE_ENV : 'development',
+    entry: path.resolve(__dirname, 'src/index.js'),
+    output: {
+
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'main.js'
+
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.(s*)css$/,
+                use: ['style-loader',
+                    {
+                        loader: 'css-loader',
+
+                        options: {
+                            modules: {
+                                mode: 'local',
+                                localIndentName: '[name]__[local]__[hash:base64:5]',
+                                auto: /\.modules\.\w+$/i
+                            }
+
+                        }
+
+                    },
+                    'sass-loader'
+                ]
+
+            }
+
+        ]
+
+    },
+
+    plugins: [ new HTMLWebpackPlugins({
+        template: path.resolve(__dirname, 'public/index.html')
+    })],
+
+    devServer: {
+        port: 3000,
+        open: true,
+        hot: true,
+    },
+    devtool: 'source-map'
+
+};
